df1c32fa7de1ea1bd7adc32df079d1beacf820b8
Randy Cullins <randycullins2018@gmail.com>
2024-04-05 15:31:20 -0600

    added mobile styles to the three card component

--- src/app/components/ThreeCardSection.js
+++ src/app/components/ThreeCardSection.js
@@ -10,7 +10,7 @@ const ThreeCardsSection = () => {
       {/* Cards */}
       <div className="flex flex-col items-center">
         {/* Card 1 */}
-        <div className="w-4/5 sm:w-40p mb-4">
+        <div className="w-full sm:w-4/5 md:w-80p lg:w-60p xl:w-50p mb-4">
           <div className="bg-gray-200 rounded-md shadow-lg p-6">
             {/* Row 1 */}
             <div className="flex justify-between items-center mb-4">
@@ -20,11 +20,13 @@ const ThreeCardsSection = () => {
               </div>
               {/* Column 2 */}
               <div className="text-center">
-                <h2 className="font-bold">Tuesday, October 3rd 2023</h2>
+                <h2 className="font-bold text-lg md:text-xl">
+                  Tuesday, October 3rd 2023
+                </h2>
                 <p>9:00 am - 2:00 pm</p>
               </div>
               {/* Column 3 (Button) */}
-              <div>
+              <div className="flex justify-end">
                 <button
                   className="py-2 px-4 rounded-md"
                   style={{ backgroundColor: "#e7cbaf" }}
@@ -36,7 +38,7 @@ const ThreeCardsSection = () => {
           </div>
         </div>
         {/* Card 2 */}
-        <div className="w-4/5 sm:w-65p mb-4">
+        <div className="w-full sm:w-4/5 md:w-80p lg:w-60p xl:w-50p mb-4">
           <div className="bg-gray-200 rounded-md shadow-lg p-6">
             {/* Row 1 */}
             <div className="flex justify-between items-center mb-4">
@@ -46,11 +48,13 @@ const ThreeCardsSection = () => {
               </div>
               {/* Column 2 */}
               <div className="text-center">
-                <h2 className="font-bold">Tuesday, October 3rd 2023</h2>
+                <h2 className="font-bold text-lg md:text-xl">
+                  Tuesday, October 3rd 2023
+                </h2>
                 <p>9:00 am - 2:00 pm</p>
               </div>
               {/* Column 3 (Button) */}
-              <div>
+              <div className="flex justify-end">
                 <button
                   className="py-2 px-4 rounded-md"
                   style={{ backgroundColor: "#e7cbaf" }}
@@ -62,7 +66,7 @@ const ThreeCardsSection = () => {
           </div>
         </div>
         {/* Card 3 */}
-        <div className="w-4/5 sm:w-65p mb-4">
+        <div className="w-full sm:w-4/5 md:w-80p lg:w-60p xl:w-50p mb-4">
           <div className="bg-gray-200 rounded-md shadow-lg p-6">
             {/* Row 1 */}
             <div className="flex justify-between items-center mb-4">
@@ -72,11 +76,13 @@ const ThreeCardsSection = () => {
               </div>
               {/* Column 2 */}
               <div className="text-center">
-                <h2 className="font-bold">Tuesday, October 3rd 2023</h2>
+                <h2 className="font-bold text-lg md:text-xl">
+                  Tuesday, October 3rd 2023
+                </h2>
                 <p>9:00 am - 2:00 pm</p>
               </div>
               {/* Column 3 (Button) */}
-              <div>
+              <div className="flex justify-end">
                 <button
                   className="py-2 px-4 rounded-md"
                   style={{ backgroundColor: "#e7cbaf" }}
